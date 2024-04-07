9fa59d2132ab17cb96964612e80a05103ad58925
jyotindra kushwah <jyotindra2421@gmail.com>
2024-04-07 20:52:25 +0530

    add typewriper

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,77 +1,131 @@
-/*===== MENU SHOW =====*/ 
-const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
 
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
-            nav.classList.toggle('show')
-        })
-    }
-}
-showMenu('nav-toggle','nav-menu')
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+    });
+  }
+};
+showMenu("nav-toggle", "nav-menu");
 
 /*===== ACTIVE AND REMOVE MENU =====*/
-const navLink = document.querySelectorAll('.nav__link');   
+const navLink = document.querySelectorAll(".nav__link");
 
-function linkAction(){
+function linkAction() {
   /*Active link*/
-  navLink.forEach(n => n.classList.remove('active'));
-  this.classList.add('active');
-  
+  navLink.forEach((n) => n.classList.remove("active"));
+  this.classList.add("active");
+
   /*Remove menu mobile*/
-  const navMenu = document.getElementById('nav-menu')
-  navMenu.classList.remove('show')
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show");
 }
-navLink.forEach(n => n.addEventListener('click', linkAction));
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*===== TYPEWRITER EFFECT =====*/
+let TxtType = function (el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = "";
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function () {
+  let i = this.loopNum % this.toRotate.length;
+  let fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+  let that = this;
+  let delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) {
+    delta /= 2;
+  }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function () {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function () {
+  let elements = document.getElementsByClassName("typewrite");
+  for (let i = 0; i < elements.length; i++) {
+    let toRotate = elements[i].getAttribute("data-type");
+    let period = elements[i].getAttribute("data-period");
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  let css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fca311}";
+  document.body.appendChild(css);
+};
 
 /*===== SCROLL REVEAL ANIMATION =====*/
 const sr = ScrollReveal({
-    origin: 'right',
-    distance: '80px',
-    duration: 2000,
-    
+  origin: "right",
+  distance: "80px",
+  duration: 2000,
 });
 
 /*SCROLL HOME*/
-sr.reveal('.home__title',{}); 
-sr.reveal('.button',{delay: 200}); 
+sr.reveal(".home__title", {});
+sr.reveal(".button", { delay: 200 });
 // sr.reveal('.home__img',{delay: 400});
-sr.reveal('.home__img', { scale: 30});
+sr.reveal(".home__img", { scale: 30 });
 // sr.reveal('.home__img', {
 //     rotate: {
 //         x: 50,
 //         z: 20
 //     }
-// }); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
+// });
+sr.reveal(".home__social-icon", { interval: 200 });
 
 /*SCROLL ABOUT*/
-sr.reveal('.about__img',{}); 
-sr.reveal('.about__subtitle',{delay: 400}); 
-sr.reveal('.about__text',{delay: 400}); 
+sr.reveal(".about__img", {});
+sr.reveal(".about__subtitle", { delay: 400 });
+sr.reveal(".about__text", { delay: 400 });
 
 /*SCROLL EDUCATION*/
-sr.reveal('.education_content',{});
-sr.reveal('.education_year',{delay:400});
-sr.reveal('.education_university',{delay:400});
-sr.reveal('.education_time',{delay:400});
-sr.reveal('.education_rounder',{delay:400});
-sr.reveal('.education_line',{delay:200});
-sr.reveal('.education_race',{delay:200});
-sr.reveal('."education_speciality',{delay:200});
-
+sr.reveal(".education_content", {});
+sr.reveal(".education_year", { delay: 400 });
+sr.reveal(".education_university", { delay: 400 });
+sr.reveal(".education_time", { delay: 400 });
+sr.reveal(".education_rounder", { delay: 400 });
+sr.reveal(".education_line", { delay: 200 });
+sr.reveal(".education_race", { delay: 200 });
+sr.reveal('."education_speciality', { delay: 200 });
 
 /*SCROLL SKILLS*/
-sr.reveal('.skills__subtitle',{});  
-sr.reveal('.skill',{interval: 200}); 
+sr.reveal(".skills__subtitle", {});
+sr.reveal(".skill", { interval: 200 });
 
 /*SCROLL WORK*/
-sr.reveal('.work__img',{interval: 200}); 
+sr.reveal(".work__img", { interval: 200 });
 
 /*SCROLL CONTACT*/
-sr.reveal('.contact__input',{interval: 200}); 
-
-
-
-
+sr.reveal(".contact__input", { interval: 200 });
